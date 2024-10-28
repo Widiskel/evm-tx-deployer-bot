874f48fabfc77967d61686c74c37e92d25bfd4d0
widiskel <waputra16@gmail.com>
2024-10-28 10:19:35 +0700

    update uncomment ty catch

--- index.js
+++ index.js
@@ -10,80 +10,80 @@ async function operation(acc) {
   await sqlite.connectToDatabase();
   await sqlite.createTable();
   const core = new Core(acc);
-  // try {
-  await core.connectWallet();
-  await core.getBalance();
+  try {
+    await core.connectWallet();
+    await core.getBalance();
 
-  if (core.balance.ETH < 0.0015)
-    throw Error("Minimum Eth Balance Is 0.0015 ETH");
-  if (Config.USEWRAPUNWRAP ?? true)
-    for (const count of Array(Config.WRAPUNWRAPCOUNT)) {
-      if (core.balance.ETH < 0.0015)
-        throw Error(
-          "Balance is less than 0.0015 ETH, please fill up your balance"
-        );
-      try {
-        await core.deposit();
-        await core.withdraw();
-        await sqlite.insertData(core.address, new Date().toISOString(), "tx");
-      } catch (error) {
+    if (core.balance.ETH < 0.0015)
+      throw Error("Minimum Eth Balance Is 0.0015 ETH");
+    if (Config.USEWRAPUNWRAP ?? true)
+      for (const count of Array(Config.WRAPUNWRAPCOUNT)) {
+        if (core.balance.ETH < 0.0015)
+          throw Error(
+            "Balance is less than 0.0015 ETH, please fill up your balance"
+          );
+        try {
+          await core.deposit();
+          await core.withdraw();
+          await sqlite.insertData(core.address, new Date().toISOString(), "tx");
+        } catch (error) {
+          await Helper.delay(
+            3000,
+            acc,
+            `Error during deposit/withdraw operation: ${error.message}`,
+            core
+          );
+        }
+        const delay = Helper.random(10000, 60000 * 2);
         await Helper.delay(
-          3000,
+          delay,
           acc,
-          `Error during deposit/withdraw operation: ${error.message}`,
+          `Delaying for ${Helper.msToTime(delay)} Before Executing Next TX`,
           core
         );
       }
-      const delay = Helper.random(10000, 60000 * 2);
+
+    if (
+      (Config.USERAWTXDATA ?? false) &&
+      (Config.RAWTX ?? undefined) !=
+        {
+          CONTRACTTOINTERACT: "CONTRACTADDRESSTOINTERACT",
+          RAWDATA: "RAWDATA",
+        }
+    )
+      for (const tx of Array(Config.RAWTXCOUNT)) {
+        await core.rawTx();
+        await sqlite.insertData(core.address, new Date().toISOString(), "raw");
+      }
+    const delay = 60000 * 60 * 24;
+    await Helper.delay(
+      delay,
+      acc,
+      `Account ${
+        privateKey.indexOf(acc) + 1
+      } Processing Done, Delaying for ${Helper.msToTime(delay)}`,
+      core
+    );
+    await operation(acc);
+  } catch (error) {
+    if (error.message) {
       await Helper.delay(
-        delay,
+        10000,
         acc,
-        `Delaying for ${Helper.msToTime(delay)} Before Executing Next TX`,
+        `Error : ${error.message}, Retry again after 10 Second`,
+        core
+      );
+    } else {
+      await Helper.delay(
+        10000,
+        acc,
+        `Error :${JSON.stringify(error)}, Retry again after 10 Second`,
         core
       );
     }
 
-  if (
-    (Config.USERAWTXDATA ?? false) &&
-    (Config.RAWTX ?? undefined) !=
-      {
-        CONTRACTTOINTERACT: "CONTRACTADDRESSTOINTERACT",
-        RAWDATA: "RAWDATA",
-      }
-  )
-    for (const tx of Array(Config.RAWTXCOUNT)) {
-      await core.rawTx();
-      await sqlite.insertData(core.address, new Date().toISOString(), "raw");
-    }
-  const delay = 60000 * 60 * 24;
-  await Helper.delay(
-    delay,
-    acc,
-    `Account ${
-      privateKey.indexOf(acc) + 1
-    } Processing Done, Delaying for ${Helper.msToTime(delay)}`,
-    core
-  );
-  await operation(acc);
-  // } catch (error) {
-  //   if (error.message) {
-  //     await Helper.delay(
-  //       10000,
-  //       acc,
-  //       `Error : ${error.message}, Retry again after 10 Second`,
-  //       core
-  //     );
-  //   } else {
-  //     await Helper.delay(
-  //       10000,
-  //       acc,
-  //       `Error :${JSON.stringify(error)}, Retry again after 10 Second`,
-  //       core
-  //     );
-  //   }
-
-  //   await operation(acc);
-  // }
+    await operation(acc);
+  }
 }
 
 async function startBot() {
