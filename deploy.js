634e8223dc213d273fa08839bcb5c49501c2f43a
Widianto Eka saputro <97203329+Widiskel@users.noreply.github.com>
2024-11-16 12:10:50 +0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -93,7 +93,7 @@ async function deployContract(
         initialSupplyBigNumber
       );
       const estimatedGas = await wallet.provider.estimateGas({
-        ...deployTx,
+        data: deployTx,
         from: wallet.address,
       });
       console.log(`Estimated gas: ${estimatedGas.toString()}`);
